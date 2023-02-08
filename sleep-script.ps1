ea1a0aa06cdb09e979bfde4c201d2b449235308c
Kevin Nipal <4549868+knipal@users.noreply.github.com>
2023-02-07 23:06:00 -0500

    add sleep script

---
+++ sleep-script.ps1
@@ -0,0 +1,13 @@
+﻿Start-Sleep -Seconds (1 * 3600)
+
+$PowerState = [System.Windows.Forms.PowerState]::Suspend;
+
+# 2. Choose whether or not to force the power state
+$Force = $false;
+
+# 3. Choose whether or not to disable wake capabilities
+$DisableWake = $false;
+
+# Set the power state
+[System.Windows.Forms.Application]::SetSuspendState($PowerState, $Force, $DisableWake);
+
