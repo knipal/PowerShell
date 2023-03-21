32cc767e2818dbfe956c6be720152024316b3cca
Kevin Nipal <4549868+knipal@users.noreply.github.com>
2023-03-21 11:16:01 -0400

    add ping with timestamps script

---
+++ test-ping-with-timestamps.ps1
@@ -0,0 +1,17 @@
+﻿$ProgressPreference = 'SilentlyContinue'
+
+$remoteHost = Read-Host -Prompt "Enter the hostname or IP"
+Write-Host "Beginning test $remoteHost"
+while (1) {
+    $pingResult = Test-NetConnection $remoteHost -InformationLevel Quiet -WarningAction SilentlyContinue -ErrorAction Stop
+        
+    Write-Host ("{0} - " -f $(Get-Date -Format "HH:mm:ss")) -NoNewline
+
+    if ($pingResult) {
+        Write-Host "Online" -BackgroundColor Green
+    } else {
+        Write-Host "Online" -BackgroundColor Red
+    }
+
+    sleep 1
+}
